ee380a9ed29f204d3a6c8a314f562f87d0f983f1
victorrlo <42052096+victorrlo@users.noreply.github.com>
2025-03-06 22:55:20 -0300

    Refactor aim behavior and update grid shader material

--- Assets/AimBehaviour.cs
+++ Assets/AimBehaviour.cs
@@ -16,63 +16,25 @@ public class AimBehaviour : MonoBehaviour
         _gridSize = _grid.cellSize.x;
     }
 
-    public void ChangeAimPosition(InputAction.CallbackContext context)
+    public void OnMouseMove(InputAction.CallbackContext context)
     {
-        Vector2 mousePos = context.ReadValue<Vector2>();
+        if (!context.performed) return;
 
-        // Criamos um raio da câmera a partir da posição do mouse
-        Ray ray = _mainCamera.ScreenPointToRay(mousePos);
-        RaycastHit hit;
-
-        
-
-        // Definimos o layer onde a mira pode ficar (exemplo: chão)
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, _aimLayerMask))
+        Vector3Int? gridCell = GetMouseGridPosition();
+        if (gridCell.HasValue)
         {
-            // Colocamos a mira na posição exata onde o raycast bateu
-            Vector3 closestPosition = GetClosestCell(hit.point);
-            Vector3 snappedPosition = SnapToGrid(closestPosition);
-            transform.position = snappedPosition;
+            transform.position = _grid.GetCellCenterWorld(gridCell.Value);
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
         }
     }
-
-    private Vector3 SnapToGrid(Vector3 position)
+    
+    private Vector3Int? GetMouseGridPosition()
     {
-        float snappedX = Mathf.Round(position.x / _gridSize) * _gridSize;
-        float snappedZ = Mathf.Round(position.z / _gridSize) * _gridSize;
-        return new Vector3(snappedX +0.5f , 0, snappedZ +0.5f);
-    }
-
-    private Vector3 GetClosestCell(Vector3 position)
-    {
-        float x1 = Mathf.Round(position.x / _gridSize) * _gridSize;
-        float x2 = Mathf.Ceil(position.x / _gridSize) * _gridSize;
-
-        float z1 = Mathf.Round(position.z / _gridSize) * _gridSize;
-        float z2 = Mathf.Ceil(position.z / _gridSize) * _gridSize;
-
-        Vector3[] candidates = new Vector3[]
+        Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _aimLayerMask))
         {
-            new Vector3(x1, 0, z1),
-            new Vector3(x1, 0, z2),
-            new Vector3(x2, 0, z1),
-            new Vector3(x2, 0, z2),
-        };
-
-        Vector3 closestPoint = candidates[0];
-        float minDistance = Vector3.Distance(position, closestPoint);
-
-        for (int i = 1; i < candidates.Length; i++)
-        {
-            float distance = Vector3.Distance(position, candidates[i]);
-
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                closestPoint = candidates[i];
-            }
+            return _grid.WorldToCell(hit.point);
         }
-
-        return closestPoint;
-    }
+        return null;
+    }   
 }
