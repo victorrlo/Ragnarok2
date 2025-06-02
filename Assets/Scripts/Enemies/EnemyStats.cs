632aeb8ddacfcad5d4737f9054ed2a92e020e006
victorrlo <42052096+victorrlo@users.noreply.github.com>
2025-06-02 20:09:15 -0300

    enemy hp bar created and following, needs now to reflect actual monster hp

---
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStats : MonoBehaviour
+{
+    [SerializeField] GameObject _monster;
+    [SerializeField] MonsterStatsData _stats;
+    private int _currentHP;
+    [SerializeField] private GameObject _statsBar;
+    [SerializeField] private Image _healthBar;
+    private bool hasBeenDamaged = false;
+    private Camera _mainCamera;
+    [SerializeField] private Vector3 _offset = new Vector3(0, -30f, 0);
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+        _currentHP = _stats._maxHP;
+        _statsBar.SetActive(false);
+        _healthBar.gameObject.SetActive(true);
+    }
+
+    private void LateUpdate()
+    {
+        if (_monster == null || _mainCamera == null) return;
+
+        SetStatsBarBellow();
+    }
+
+    private void ShowHealthBar()
+    {
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _currentHP -= amount;
+    }
+
+    private void SetStatsBarBellow()
+    {
+        _statsBar.SetActive(true);
+        if (_mainCamera == null) _mainCamera = Camera.main;
+        
+        Vector3 screenPos = _mainCamera.WorldToScreenPoint(_monster.transform.position);
+
+        Vector3 targetPos = screenPos + _offset;
+        
+        _statsBar.transform.position = targetPos;
+    }
+}
